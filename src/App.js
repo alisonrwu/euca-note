f4125a514772ce0472f52727e96ce65c8a5608ab
Paul Wang <paul_wang347@hotmail.com>
2020-01-11 19:34:57 -0800

    Add login page skeleton

--- src/App.js
+++ src/App.js
@@ -13,17 +13,37 @@ import NotePage from './pages/NotePage';
 import NotesPage from './pages/NotesPage';
 import LoginPage from './pages/LoginPage';
 
+import * as firebase from "firebase/app";
+
+// Set up firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBz2XEj5EQ2k2H-BKCom-IHqmVSHeVXKNE",
+  authDomain: "euca-e7797.firebaseapp.com",
+  databaseURL: "https://euca-e7797.firebaseio.com",
+  projectId: "euca-e7797",
+  storageBucket: "euca-e7797.appspot.com",
+  messagingSenderId: "158884482446",
+  appId: "1:158884482446:web:ce4fe9acc745275698a8df",
+  measurementId: "G-VFF06608V9"
+};
+
+// Initialize Firebase
+firebase.default.initializeApp(firebaseConfig);
+
 function App() {
   return (
     <Router>
       <div className="App">
-        <header>
-          <Navbar bg="light" expand="sm">
-            <NavItem><Link to="/login">Login</Link></NavItem>
-            <NavItem><Link to="/notes">Notes</Link></NavItem>
-            <NavItem><Link to="/">Write</Link></NavItem>
-          </Navbar>
-        </header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/notes">Notes</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
         <body>
           <Switch>
             <Route path="/login">
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,13 +1,203 @@
-import React from 'react';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import { withRouter } from "react-router-dom";
 
-function LoginPage() {
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const LoginStateEnum = Object.freeze({ default: 1, login: 2, register: 3 });
+let googleProvider = null;
+
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { displayMode: LoginStateEnum.default };
+    this.signInGoogle = this.signInGoogle.bind(this);
+    this.showEmailLogin = this.showEmailLogin.bind(this);
+    this.showRegister = this.showRegister.bind(this);
+    this.navigateAfterAuthenticated = this.navigateAfterAuthenticated.bind(
+      this
+    );
+    this.signInEmail = this.signInEmail.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleRegister = this.handleRegister.bind(this);
+    this.userIsAuthenticated = this.userIsAuthenticated.bind(this);
+  }
+
+  navigateAfterAuthenticated() {
+    this.props.history.push("/notes");
+  }
+
+  signInGoogle() {
+    let self = this;
+    firebase.default
+      .auth()
+      .signInWithPopup(googleProvider)
+      .then(function(result) {
+        self.navigateAfterAuthenticated();
+      })
+      .catch(function(error) {
+        // TODO: error handling
+        console.log(error);
+      });
+  }
+
+  signInEmail(email, password) {
+    let self = this;
+    firebase.default
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(function(result) {
+        console.log(result);
+        self.navigateAfterAuthenticated();
+      })
+      .catch(function(error) {
+        // TODO: error handling
+        console.log(error);
+      });
+  }
+
+  handleLogin() {
+    let email = this.refs.loginEmailField.value;
+    let password = this.refs.loginPasswordField.value;
+    this.signInEmail(email, password);
+  }
+
+  handleRegister() {
+    let self = this;
+    let email = this.refs.registerEmailField.value;
+    let password = this.refs.registerPasswordField.value;
+    firebase.default
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(function(result) {
+        self.signInEmail(email, password);
+      })
+      .catch(function(error) {
+        // TODO: error handling
+        console.log(error);
+      });
+  }
+
+  userIsAuthenticated() {
+    let user = firebase.default.auth().currentUser;
+    return user !== null;
+  }
+
+  showDefault() {
+    this.setState({
+      displayMode: LoginStateEnum.default
+    });
+  }
+
+  showEmailLogin() {
+    this.setState({
+      displayMode: LoginStateEnum.login
+    });
+  }
+
+  showRegister() {
+    this.setState({
+      displayMode: LoginStateEnum.register
+    });
+  }
+
+  componentDidMount() {
+    if (this.userIsAuthenticated()) {
+      this.navigateAfterAuthenticated();
+    }
+    googleProvider = new firebase.default.auth.GoogleAuthProvider();
+    firebase.default
+      .auth()
+      .setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+  }
+
+  render() {
     return (
       <div>
-          <p>
-            TODO: login here
-          </p>
+        <Card className="m-5">
+          {this.state.displayMode === LoginStateEnum.default ? (
+            <Card.Body>
+              <h1>Sign In</h1>
+              <Container>
+                <Row className="m-2 justify-content-md-center">
+                  <Button onClick={this.signInGoogle}>Google</Button>
+                </Row>
+                <Row className="m-2 justify-content-md-center">
+                  <Button onClick={this.showEmailLogin}>Email</Button>
+                </Row>
+                <Row className="m-2 justify-content-md-center">
+                  <Button onClick={this.showRegister}>Register</Button>
+                </Row>
+              </Container>
+            </Card.Body>
+          ) : this.state.displayMode === LoginStateEnum.login ? (
+            <Card.Body>
+              <h1>Email Sign In</h1>
+              <Form>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    ref="loginEmailField"
+                    type="email"
+                    placeholder="Enter email"
+                  />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    ref="loginPasswordField"
+                    type="password"
+                    placeholder="Password"
+                  />
+                </Form.Group>
+                <Button variant="primary" onClick={this.handleLogin}>
+                  Login
+                </Button>
+              </Form>
+            </Card.Body>
+          ) : (
+            <Card.Body>
+              <h1>Register</h1>
+              <Form>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    ref="registerEmailField"
+                    type="email"
+                    placeholder="Enter email"
+                  />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    ref="registerPasswordField"
+                    type="password"
+                    placeholder="Password"
+                  />
+                </Form.Group>
+                <Button variant="primary" onClick={this.handleRegister}>
+                  Register
+                </Button>
+              </Form>
+            </Card.Body>
+          )}
+        </Card>
       </div>
     );
   }
-  
-  export default LoginPage;
+}
+
+export default withRouter(LoginPage);
