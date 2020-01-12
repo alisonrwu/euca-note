c038efd5f03f7e292919dd3e20c7f95abb154f7e
Paul Wang <paul_wang347@hotmail.com>
2020-01-12 03:13:25 -0800

    even more minor

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ class App extends React.Component {
     let self = this;
     firebase.default.auth().onAuthStateChanged(function(user) {
       self.setState({authenticated: user});
-      if (user) this.setupNotes();
+      if (user) self.setupNotes();
     });
   }
 
